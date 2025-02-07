83068c860ce3d6f1e890a7f0ce1d97c25c719660
Alvirah-Ro <bajoraneh@hotmail.com>
2025-02-06 22:27:00 -0600

    back to original idea but image still isn't loading

--- views.py
+++ views.py
@@ -6,14 +6,8 @@ from .models import Product
 # Home page view
 def index_view(request):
     newest_product_list = Product.objects.order_by("-date_rec")[:5]
-    product_name = Product.name
-    product_img = Product.image
-    product_price = Product.price
 
     # Return the context to the template
     return render(request, 'aria_music/index.html', {
         "newest_product_list": newest_product_list,
-        "product_name": product_name,
-        "product_img": product_img,
-        "product_price": product_price
     })
