921f4225230c49e83cf81cd1619e8023159d4414
Alvirah-Ro <bajoraneh@hotmail.com>
2025-02-12 17:31:45 -0600

    got product details page to work; now working on search functionality

--- urls.py
+++ urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 app_name = "aria_music"
 urlpatterns = [
     path("", views.index_view, name="index"),
-    path("product/{{ product.sku }}/", views.product_view, name="product"),
+    path("product/<str:product_sku>/", views.product_detail_view, name="product_detail"),
+    path("product/search-results/", views.product_search_view, name="product_search"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- views.py
+++ views.py
@@ -12,9 +12,17 @@ def index_view(request):
         "newest_product_list": newest_product_list
     })
 
-def product_view(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
-    
+def product_detail_view(request, product_sku):
+    product = get_object_or_404(Product, sku=product_sku)
     return render(request, 'aria_music/product.html', {
         "product": product
+    })
+
+def product_search_view(request):
+    query = request.Get.get('q', '') # Get search query from URL parameter
+    products = Product.objects.filter(name__icontains=query) if query else []
+
+    return render(request, 'aria_music/search_results.html', {
+        "query": query,
+        "products": products
     })
