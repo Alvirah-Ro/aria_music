c6ceb54dc520adf6c888fd60f8433cd11b9b1893
Alvirah-Ro <bajoraneh@hotmail.com>
2025-02-13 15:43:39 -0600

    added base.html and changed other templates to extend this page

--- urls.py
+++ urls.py
@@ -7,5 +7,5 @@ app_name = "aria_music"
 urlpatterns = [
     path("", views.index_view, name="index"),
     path("product/<str:product_sku>/", views.product_detail_view, name="product_detail"),
-    path("product/search-results/", views.product_search_view, name="product_search"),
+    path("product/search/", views.product_search_view, name="product_search"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.templatetags.static import static
 
 # Create your views here.
 
@@ -14,12 +15,14 @@ def index_view(request):
 
 def product_detail_view(request, product_sku):
     product = get_object_or_404(Product, sku=product_sku)
+    product_image_url = static(f"aria_music/{product.sku}.jpg")
     return render(request, 'aria_music/product.html', {
-        "product": product
+        "product": product,
+        "product_image_url": product_image_url
     })
 
 def product_search_view(request):
-    query = request.Get.get('q', '') # Get search query from URL parameter
+    query = request.GET.get("q", "") # Get search query from URL parameter
     products = Product.objects.filter(name__icontains=query) if query else []
 
     return render(request, 'aria_music/search_results.html', {
