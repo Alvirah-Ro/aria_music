1a0164102251e3400afa1a140dcbcf54379a20e6
Alvirah-Ro <bajoraneh@hotmail.com>
2025-02-13 16:14:17 -0600

    search is starting to work but still need to fix how images are loading

--- urls.py
+++ urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 app_name = "aria_music"
 urlpatterns = [
     path("", views.index_view, name="index"),
-    path("product/<str:product_sku>/", views.product_detail_view, name="product_detail"),
     path("product/search/", views.product_search_view, name="product_search"),
+    path("product/<str:product_sku>/", views.product_detail_view, name="product_detail"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
