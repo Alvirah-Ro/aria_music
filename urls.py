3e1a6cd89e165fa6b03464868e6b010a3eef1b57
Alvirah-Ro <bajoraneh@hotmail.com>
2025-02-17 08:26:15 -0600

    fixed product link so main page is loading

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.conf import settings
-from . import views
 from django.conf.urls.static import static
+from . import views
 
 app_name = "aria_music"
 urlpatterns = [
